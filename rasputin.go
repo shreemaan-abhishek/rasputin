7d9c8a90a578c541a984d6586202108a03715bda
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2022-11-05 23:16:07 +0530

    inital code for leader election campaign

--- rasputin.go
+++ rasputin.go
@@ -1,7 +1,51 @@
 package rasputin
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-func New() {
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+)
+
+var (
+	name string
+	cli *clientv3.Client
+	electionSession *concurrency.Session
+	election *concurrency.Election
+	ctx *context.Context
+	pfx string
+	val string
+)
+
+func commission(candidateName string, client *clientv3.Client, leaseTimeToLive int, electionPrefix string, electionContext context.Context, value string) {
+	name = candidateName
+	cli = client
+	pfx = electionPrefix
+	ctx = &electionContext
+	val = value
+
+	s, err := concurrency.NewSession(cli, concurrency.WithTTL(leaseTimeToLive))
+	if err != nil {
+	  log.Fatal(err)
+	}
+	electionSession = s
+	election = concurrency.NewElection(electionSession, pfx)
+
 	fmt.Println("Rasputin!")
+}
+
+func participate() {
+	if err := election.Campaign(*ctx, pfx); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("leader election for", name)
+	fmt.Println("Do some work in", name)
+	time.Sleep(500 * time.Second)  
+	if err := election.Resign(*ctx); err != nil {
+	  log.Fatal(err)
+	}
+	fmt.Println("resign ", name)
 }
