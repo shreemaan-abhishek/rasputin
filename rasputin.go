79d7eb64fdf2e0df4de338d8cfb14b332c307972
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2022-11-10 03:36:39 +0530

    add logic for checking leadership status

--- rasputin.go
+++ rasputin.go
@@ -11,14 +11,15 @@ import (
 )
 
 var (
-	name            string
-	cli             *clientv3.Client
-	electionSession *concurrency.Session
-	election        *concurrency.Election
-	ctx             *context.Context
-	pfx             string
-	val             string
-	ldrshipDuration time.Duration
+	name             string
+	cli              *clientv3.Client
+	electionSession  *concurrency.Session
+	election         *concurrency.Election
+	ctx              *context.Context
+	pfx              string
+	val              string
+	ldrshipDuration  time.Duration
+	currentLeaderKey []byte
 )
 
 func Commission(candidateName string, client *clientv3.Client, leaseTimeToLive int, electionPrefix string, electionContext *context.Context, value string, leadershipDuration time.Duration) {
@@ -37,8 +38,22 @@ func Commission(candidateName string, client *clientv3.Client, leaseTimeToLive i
 	election = concurrency.NewElection(electionSession, pfx)
 
 	go watch(electionContext)
+	go observe()
 
 	fmt.Println("Rasputin!")
+}
+
+func observe() {
+	cres := election.Observe(context.Background())
+	for {
+		response := <-cres
+		currentLeaderKey = response.Kvs[0].Key
+	}
+}
+
+func IsLeader() bool {
+	//TODO: doc
+	return election.Key() == string(currentLeaderKey)
 }
 
 func Participate() {
